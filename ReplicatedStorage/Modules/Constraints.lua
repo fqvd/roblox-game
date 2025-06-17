96cc6584b08795c4dc95468f780acd5e49194a76
Evgeniy <151476924+fqvd@users.noreply.github.com>
2025-06-17 18:02:20 +0300

    Update Constraints.lua

--- ReplicatedStorage/Modules/Constraints.lua
+++ ReplicatedStorage/Modules/Constraints.lua
@@ -1,47 +1,33 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Constraints = {}
 
-local Signal = require(ReplicatedStorage.Modules.Signal)
+function Constraints.motor6D(p0, p1, name)
+	local motor = Instance.new("Motor6D")
+	motor.Part0 = p0
+	motor.Part1 = p1
+	motor.Name  = name or p1.Name
+	motor.Parent = p0
 
-
-local Cooldown = {}
-Cooldown.__index = Cooldown
-
-function Cooldown.new(initial)
-	local self = setmetatable({}, Cooldown)
-    self.cooldown = initial
-	self._time = os.clock() - initial
-
-	self.CooldownUpdated = Signal.new()
-
-	return self
+	return motor
 end
 
-function Cooldown:IsFinished()
-	local now = os.clock()
-	return now - self._time >= self.cooldown
+function Constraints.weldConstraint(p0, p1, name)
+	local weld = Instance.new("WeldConstraint")
+	weld.Part0 = p0
+	weld.Part1 = p1
+	weld.Name  = name or p1.Name
+	weld.Parent = p0
+
+	return weld
 end
 
-function Cooldown:Update()
-	local now = os.clock()
-	self._time = now
-
-	self.CooldownUpdated:Fire()
+function Constraints.weld(p0, p1, name)
+	local weld = Instance.new("Weld")
+	weld.Part0 = p0
+	weld.Part1 = p1
+	weld.Name  = name or p1.Name
+	weld.Parent = p0
+	
+	return weld
 end
 
-function Cooldown:SetNewCooldown(cooldown)
-	cooldown = cooldown or self.cooldown
-
-	self._time = os.clock()
-	self.cooldown = cooldown
-end
-
-function Cooldown:GetRemainingTime()
-	local delta = os.clock() - self._time
-	return math.clamp(self.cooldown - delta, 0, self.cooldown)
-end
-
-function Cooldown:Finish()
-	self._time = os.clock() - self.cooldown
-end
-
-return Cooldown
+return Constraints
