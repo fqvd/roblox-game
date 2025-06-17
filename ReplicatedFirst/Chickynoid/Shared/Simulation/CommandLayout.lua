31cd796a9129275d30f04866f27e3f0154e848c4
Evgeniy <151476924+fqvd@users.noreply.github.com>
2025-06-17 17:33:32 +0300

    Create CommandLayout.lua

---
+++ ReplicatedFirst/Chickynoid/Shared/Simulation/CommandLayout.lua
@@ -0,0 +1,42 @@
+local module = {}
+ 
+
+local CrunchTable = require(script.Parent.Parent.Vendor.CrunchTable)
+
+function module:GetCommandLayout()
+	
+	if (self.commandLayout == nil) then
+		self.commandLayout = CrunchTable:CreateLayout()	
+			
+		self.commandLayout:Add("localFrame",CrunchTable.Enum.INT32)
+		self.commandLayout:Add("serverTime", CrunchTable.Enum.FLOAT)
+		self.commandLayout:Add("deltaTime", CrunchTable.Enum.FLOAT)
+		self.commandLayout:Add("snapshotServerFrame", CrunchTable.Enum.INT32)	
+		self.commandLayout:Add("playerStateFrame", CrunchTable.Enum.INT32)
+		self.commandLayout:Add("shiftLock", CrunchTable.Enum.UBYTE)
+		self.commandLayout:Add("x", CrunchTable.Enum.FLOAT)
+		self.commandLayout:Add("y", CrunchTable.Enum.FLOAT)
+		self.commandLayout:Add("z", CrunchTable.Enum.FLOAT)
+		self.commandLayout:Add("fa", CrunchTable.Enum.VECTOR3)
+		self.commandLayout:Add("f", CrunchTable.Enum.FLOAT)
+		self.commandLayout:Add("j", CrunchTable.Enum.FLOAT)
+
+		self.commandLayout:Add("j", CrunchTable.Enum.FLOAT)
+
+		self.commandLayout:Add("sprinting", CrunchTable.Enum.UBYTE)
+		self.commandLayout:Add("tackleDir", CrunchTable.Enum.VECTOR3)
+		self.commandLayout:Add("diveDir", CrunchTable.Enum.VECTOR3)
+	end
+	
+	return self.commandLayout	
+end
+
+function module:EncodeCommand(command)
+	return CrunchTable:BinaryEncodeTable(command, self:GetCommandLayout())
+end
+
+function module:DecodeCommand(command)
+	return CrunchTable:BinaryDecodeTable(command, self:GetCommandLayout()) 
+end
+
+return module
