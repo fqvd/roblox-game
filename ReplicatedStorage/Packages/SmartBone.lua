578d37ef34a4377427ee5d390bdc3e7e025ed65a
Evgeniy <151476924+fqvd@users.noreply.github.com>
2025-06-17 18:09:26 +0300

    Create SmartBone.lua

---
+++ ReplicatedStorage/Packages/SmartBone.lua
@@ -0,0 +1,6 @@
+local REQUIRED_MODULE = require(script.Parent._Index["jakeywastaken_smartbone-2@0.4.2"]["smartbone-2"])
+export type IBoneTree = REQUIRED_MODULE.IBoneTree 
+export type IBone = REQUIRED_MODULE.IBone 
+export type IColliderObject = REQUIRED_MODULE.IColliderObject 
+export type IColliderTable = REQUIRED_MODULE.IColliderTable 
+return REQUIRED_MODULE
