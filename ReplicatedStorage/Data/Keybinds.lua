02685280dfa922a4fb7de4683be78c660c8fa0d6
Evgeniy <151476924+fqvd@users.noreply.github.com>
2025-06-17 17:59:08 +0300

    Create Keybinds.lua

---
+++ ReplicatedStorage/Data/Keybinds.lua
@@ -0,0 +1,28 @@
+return {
+    PC = {
+        Dive = Enum.UserInputType.MouseButton2,
+        Shoot = Enum.UserInputType.MouseButton1,
+
+        Tackle = Enum.KeyCode.E,
+        Skill = Enum.KeyCode.Q,
+
+        RequestBall = Enum.KeyCode.R,
+        Sprint = Enum.KeyCode.LeftShift,
+        ShiftLock = Enum.KeyCode.LeftControl,
+
+        Emote = Enum.KeyCode.B,
+    },
+    Console = {
+        Shoot = Enum.KeyCode.ButtonR2,
+
+        Dive = Enum.KeyCode.ButtonL1,
+        Tackle = Enum.KeyCode.ButtonL1,
+        Skill = Enum.KeyCode.ButtonL1,
+
+        RequestBall = Enum.KeyCode.ButtonR1,
+        Sprint = Enum.KeyCode.ButtonY,
+
+        Emote = Enum.KeyCode.DPadLeft,
+        ShiftLock = Enum.KeyCode.ButtonL3,
+    }
+}
