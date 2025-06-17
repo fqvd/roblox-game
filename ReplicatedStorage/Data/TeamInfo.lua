8d64767f2ec55464750445b55dbb94e3ff783e9f
Evgeniy <151476924+fqvd@users.noreply.github.com>
2025-06-17 17:59:20 +0300

    Create TeamInfo.lua

---
+++ ReplicatedStorage/Data/TeamInfo.lua
@@ -0,0 +1,15 @@
+return {
+
+	["Team 1"] = {
+		["Text"] = "Team 1",
+		["Identifier"] = "White",
+		["MainColor"] = Color3.fromRGB(255, 255, 255),
+	},
+
+	["Team 2"] = {
+		["Text"] = "Team 2",
+		["Identifier"] = "Red",
+		["MainColor"] = Color3.fromRGB(139, 22, 37),
+	},
+
+}
